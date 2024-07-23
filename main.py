4bcf62d69b1e419a664a2fc1d22083886d5abde3
MAB-100 <shahabbas661@gmail.com>
2024-07-23 17:26:42 +0500

    pdf from csv

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fpdf import FPDF
+import pandas as pd
+
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
-pdf.add_page()
 
-pdf.set_font(family="Times",style="B", size=12)
-pdf.cell(w=0, h=10, txt="Hello World", border=1, ln=1, align='L')
-pdf.cell(w=0, h=10, txt="Hey there", border=1, ln=1, align='L')
+df = pd.read_csv('topics.csv')
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times",style="B", size=24)
+    pdf.set_text_color(100, 100, 100)  
+    pdf.cell(w=0, h=10, txt=row['Topic'], ln=1, align='L')
+    pdf.line(10, 20, 200, 20)
+
 pdf.output('output.pdf')
+
